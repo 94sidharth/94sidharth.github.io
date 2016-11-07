26978d9aa31de08ad27f43d88891398b30b205fc
Sidharth <94sidharth@gmail.com>
2016-11-07 10:49:35 +0530

    speedfour

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports=function(grunt){
 		imagemin: {
 			static: {
 				options: {
-					optimizationLevel : 7,
+					optimizationLevel : 7
 					//svgoPlugins: [{removeViewBox: false}],
 					//use: [mozjpeg()]
 				},
